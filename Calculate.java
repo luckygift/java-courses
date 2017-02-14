2e20716dc260e61a2e00b84624f4167b7d3cffe3
luckygift <luckygift@mail.ru>
2017-02-15 00:42:40 +0200

    test test ok~! ---

--- Calculate.java
+++ Calculate.java
@@ -2,4 +2,10 @@ public class Calculate {
 public static void main (String[] arg) {
 System.out.println("Calculate...!!!");
 }
+
+ System.out.println("Hellow!");
+        System.out.println("Hellow!");
+         System.out.println("Hellow!");
+        System.out.println("Hellow!");
+
 }
