d4634791ee1eaa5eaf425ec9642844b7b052af1f
luckygift <luckygift@mail.ru>
2017-04-17 20:43:33 +0300

    test44444444!

--- Calculate.java
+++ Calculate.java
@@ -25,6 +25,21 @@ public static void main (String[] arg) {
     System.out.println("Hellow!");
 
 
+
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+
+
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+    System.out.println("Hellow!");
+
+
 }
 }
 
